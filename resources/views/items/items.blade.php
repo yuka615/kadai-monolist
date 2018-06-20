22f5f48668e50d4208b5179ba60fdff4c16ab01d
yuka.yanagawa <yuka.yanagawa@rakuten.com>
2018-06-20 07:12:39 +0000

    have

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -16,10 +16,10 @@
                             <div class="buttons text-center">
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
+                                    @include('items.have_button', ['item' => $item])
                                 @endif
                             </div>
-                        </div>
-                    </div>
+                            
                 </div>
             </div>
         @endforeach
--- resources/views/items/want_button.blade.php
+++ resources/views/items/want_button.blade.php
@@ -1,7 +1,7 @@
 @if (Auth::user()->is_wanting($item->code))
     {!! Form::open(['route' => 'item_user.dont_want', 'method' => 'delete']) !!}
         {!! Form::hidden('itemCode', $item->code) !!}
-        {!! Form::submit('Want', ['class' => 'btn btn-success']) !!}
+        {!! Form::submit('Added', ['class' => 'btn btn-success']) !!}
     {!! Form::close() !!}
 @else
     {!! Form::open(['route' => 'item_user.want']) !!}
