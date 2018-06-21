d9843d05b8e717152e9fb75e5ce02450865d56fe
yuka.yanagawa <yuka.yanagawa@rakuten.com>
2018-06-21 02:43:38 +0000

    ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,6 +1,6 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
@@ -19,9 +19,16 @@
                                     @include('items.have_button', ['item' => $item])
                                 @endif
                             </div>
-                        </div>    
+                        </div>
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}}</p>
+                            </div>
+                        @endif
+                    </div>
                 </div>
             </div>
         @endforeach
     </div>
 @endif
+
